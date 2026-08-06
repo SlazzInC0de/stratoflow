44ad9c114bafb26baa17a11276846a854f44890a
SlazzInC0de <devslazz@gmail.com>
2026-08-06 14:31:16 +0530

    Phase 4: fix Terraform formatting

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "synapse_storage_reader" {
   scope                = azurerm_storage_account.main.id
   role_definition_name = "Storage Blob Data Reader"
-  principal_id          = azurerm_synapse_workspace.main.identity[0].principal_id
+  principal_id         = azurerm_synapse_workspace.main.identity[0].principal_id
 }
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,15 +1,15 @@
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
-  location = "eastus"  # matches your existing group's region
+  location = "eastus" # matches your existing group's region
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-allow_nested_items_to_be_public  = true
+  name                             = var.storage_account_name
+  resource_group_name              = azurerm_resource_group.main.name
+  location                         = var.location
+  account_tier                     = "Standard"
+  account_replication_type         = "LRS"
+  allow_nested_items_to_be_public  = true
   cross_tenant_replication_enabled = true
   min_tls_version                  = "TLS1_2"
 }
